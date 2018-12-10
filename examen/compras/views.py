c953d7259afca5af1e6eeca0b769b9c728b7267f
Pablo, Diego y Nicolas <pablo.ignacio288@gmail.com>
2018-12-09 22:04:48 -0300

    login

---
+++ examen/compras/views.py
@@ -0,0 +1,123 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Productos, Tienda
+from django.http import HttpResponse
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistroForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.http import HttpResponse
+# Create your views here.
+class InicioView(TemplateView):
+    template_name = 'compras/inicio.html'
+
+def index(request):
+    return render(request, 'compras/inicio.html', {})
+
+def base_layout(request):
+	template='compras/inicio.html'
+	return render(request,template)
+
+def getdata(request):
+	results=feed.objects.all()
+	jsondata = serializers.serialize('json',results)
+	return HttpResponse(jsondata)
+
+class ProductosList(ListView):
+    model = Productos
+
+class ProductosView(DetailView):
+    model = Productos
+
+class ProductosCreate(CreateView):
+    model = Productos
+    fields = ['nombre', 'costo_presupuesto', 'costo_real', 'tienda', 'notas']
+    success_url = reverse_lazy('productos_list')
+
+class ProductosUpdate(UpdateView):
+    model = Productos
+    fields = ['nombre', 'costo_presupuesto', 'costo_real', 'tienda', 'notas']
+    success_url = reverse_lazy('productos_list')
+
+class ProductosDelete(DeleteView):
+    model = Productos
+    success_url = reverse_lazy('productos_list')
+
+class ProductosForm(ModelForm):
+    class Meta:
+        model = Productos
+        fields = ['nombre', 'costo_presupuesto', 'costo_real', 'tienda', 'notas']
+
+def productos_list(request, template_name='compras/productos_list.html'):
+    productos = Productos.objects.all()
+    data = {}
+    data['object_list'] = productos
+    return render(request, template_name, data)
+
+def productos_view(request, pk, template_name='compras/productos_detail.html'):
+    productos= get_object_or_404(Productos, pk=pk)    
+    return render(request, template_name, {'object':productos})
+
+def productos_create(request, template_name='compras/productos_form.html'):
+    form = ProductosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('productos_list')
+    return render(request, template_name, {'form':form})
+
+def productos_update(request, pk, template_name='compras/productos_form.html'):
+    productos= get_object_or_404(Productos, pk=pk)
+    form = ProductosForm(request.POST or None, instance=productos)
+    if form.is_valid():
+        form.save()
+        return redirect('producto_list')
+    return render(request, template_name, {'form':form})
+
+def productos_delete(request, pk, template_name='compras/productos_confirm_delete.html'):
+    productos= get_object_or_404(Productos, pk=pk)    
+    if request.method=='POST':
+        productos.delete()
+        return redirect('productos_list')
+    return render(request, template_name, {'object':productos})
+
+class TiendaList(ListView):
+    model = Tienda
+
+class TiendaView(DetailView):
+    model = Tienda
+
+class TiendaCreate(CreateView):
+    moodel = Tienda
+    fields = ['nombre', 'sucursal', 'direccion', 'ciudad', 'region']
+    success_url = reverse_lazy('tienda_list')
+
+class TiendaUpdate(UpdateView):
+    model = Tienda
+    fields = ['nombre', 'sucursal', 'direccion', 'ciudad', 'region']
+    success_url = reverse_lazy('tienda_list')
+
+class TiendaDelete(DeleteView):
+    model = Tienda
+    success_url = reverse_lazy('tienda_list')
+
+class InfoView(TemplateView):
+    template_name = 'compras/quienes_somos.html'
+
+class ServicioView(TemplateView):
+    template_name = 'compras/servicios.html'
+
+class RegistroUsuario(CreateView):
+    model = User
+    template_name = 'compras/registro.html'
+    form_class = RegistroForm
+    success_url= reverse_lazy('productos_list')
+
+class SignInView(LoginView):
+    template_name = 'compras/login.html'
+
+class SignOutView(LogoutView):
+    pass
---
+++ examen/examen/urls.py
@@ -0,0 +1,30 @@
+"""examen URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include, url
+from compras.views import InfoView, ServicioView, InicioView, RegistroUsuario, SignInView, SignOutView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+   	url(r'', include('compras.urls')),
+    url(r'^quienes_somos/$', InfoView.as_view(), name='somos'),
+    url(r'^servicios/$', ServicioView.as_view(), name='servicio'),
+    url(r'^inicio/$', InicioView.as_view(), name='inicio'),
+    url(r'^registro', RegistroUsuario.as_view(), name="registrar"),
+    url(r'^inicia-sesion/$', SignInView.as_view(), name='sign_in'),
+    url(r'^cerrar-sesion/$', SignOutView.as_view(), name='sign_out'),
+]
